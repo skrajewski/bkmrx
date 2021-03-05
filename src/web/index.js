74c7ea94492b43b7dc874d3c5af3f738928944ef
Szymon Krajewski <szykra@gmail.com>
2021-03-05 21:13:45 +0100

    Fix path to assets

--- src/web/index.js
+++ src/web/index.js
@@ -3,11 +3,12 @@ const api = require('./api');
 const browser = require('./browser');
 const app = express();
 const mustacheExpress = require('mustache-express');
+const path = require('path');
 
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
-app.set('views', `${__dirname}/../../views`);
-app.use(express.static('assets'));
+app.set('views', path.normalize(`${__dirname}/../../views`));
+app.use(express.static(path.normalize(`${__dirname}/../../assets`)));
 
 app.use('/', browser);
 app.use('/api', api);
